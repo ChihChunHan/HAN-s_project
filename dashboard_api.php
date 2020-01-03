276bb9bbba290bd6e053b1d25a46f397dd3f2a74
ChihChunHan <chihchun.han@gmail.com>
2020-01-03 16:53:21 +0800

    update

--- dashboard_api.php
+++ dashboard_api.php
@@ -14,17 +14,17 @@ switch ($_GET['nav']) {
             //卡片主體
             echo '
             <a class="col-lg-3 col-md-4 col-12 mb-4 text-reset" data-toggle="modal" data-backdrop="false" href="#c'.$row['id'].'">
-            <div class="card">
-                <img src="'.$row['img'].'" class="card-img-top">
+            <div class="card border-0 shadow">
+                <img src="'.$row['img'].'" class=" card-img-top card-img-size" style="object-fit:cover">
                 <div class="card-body">
-                    <h5 class="card-title">'.$row['title'].'</h5>
+                    <h5 class="card-title" style="font-size: 1rem;">'.$row['title'].'</h5>
                     <p>';
             foreach ($tags as $tag) {  // 輸出tag
                 echo '<span class="badge badge-secondary mr-1">'.$tag.'</span>';
             }
             echo '
                     </p>
-                    <p class="card-text">'.$row['content'].'</p>
+                    <p class="card-text" style="font-size: 0.8rem;">'.$row['content'].'</p>
                     <p class="card-text"><small class="text-muted">'.$row['date'].'</small></p>
                 </div>
             </div>';
@@ -34,7 +34,7 @@ switch ($_GET['nav']) {
 
             <div class="modal" id="c'.$row['id'].'" tabindex="-1" style="background-color:rgb(255,255,255,0.7)">
             <div class="modal-dialog modal-dialog-centered modal-lg">
-                <div class="modal-content">
+                <div class="modal-content border-0 shadow">
                 <div class="modal-header">
                     <h5 class="modal-title" id="c'.$row['id'].'Title">修改作品資訊</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -45,24 +45,28 @@ switch ($_GET['nav']) {
                     <form>
                     <div class="container-fluid">
                         <div class="row">
-                        <div class="col-6">
+                        <div class="col-md-6 col-12">
                             <img src="'.$row['img'].'" class="card-img-top w-100">
                         </div>
-                        <div class="col-6">
+                        <div class="col-md-6 col-12">
                             <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" value="'.$row['title'].'">
-                            <small id="emailHelp" class="form-text text-muted">Well never share your email with anyone else.</small>
+                            <label>title</label>
+                            <input type="text" class="form-control" value="'.$row['title'].'">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                <label>tag</label>
+                                <input type="text" class="form-control">
+                                <small class="form-text text-muted">';
+                                foreach ($tags as $tag) {  // 輸出tag
+                                    echo '<span class="badge badge-secondary mr-1">'.$tag.'<a href="#" class="text-reset">&times;</a></span>';
+                                };
+                                echo'</small>
                             </div>
                             <div class="form-group">
-                                <label for="exampleFormControlTextarea1">Example textarea</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                <label>text content</label>
+                                <textarea class="form-control" rows="3">'.$row['content'].'</textarea>
                             </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" class="btn btn-secondary">Submit</button>
                         </div>
                         </div>
                     </div>
