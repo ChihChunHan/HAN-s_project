2242853504ca70fdab3aa32e347d38339df07de0
ChihChunHan <chihchun.han@gmail.com>
2020-01-09 00:24:35 +0800

    fix user info

--- explore.php
+++ explore.php
@@ -124,130 +124,128 @@ if (!empty($_GET['search'])) {echo "$.getJSON('explore_api.php?search=".$_GET['s
 else {echo "$.getJSON('explore_api.php?do=init').done(showCard)";}
 ?>
 
-
     function showCard(e){
       let data = e
       for (let i = 0; i < data.length; i++) {
-        let userData
+        let str,userData
         $.getJSON('explore_user_api.php?user='+data[i].acc).done(function (e) {
           userData = e
-          console.log(userData);
-        })
-        let str=`
-        <div class="grid-item">
-          <div class="card border-0" style="width: 200px;">
-            <a href="#e_c${+i}" data-toggle="modal" data-backdrop="true"></a>
-            <div class="card-body">
-              <h5 class="card-title m-0 mb-1 ml-n1" style="font-size: 1rem;">${data[i].title}</h5>
-              <p class="card-text m-0 text-truncate" style="font-size: 0.5rem;">${data[i].content}</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="modal" id="e_c${+i}" tabindex="-1" style="background-color:rgb(255,255,255,0.5)">
-          <div class="modal-dialog modal-dialog-centered mx-auto modal-lg">
-              <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">${data[i].title}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          str=`
+            <div class="grid-item">
+              <div class="card border-0" style="width: 200px;">
+                <a href="#e_c${+i}" data-toggle="modal" data-backdrop="true"></a>
+                <div class="card-body">
+                  <h5 class="card-title m-0 mb-1 ml-n1" style="font-size: 1rem;">${data[i].title}</h5>
+                  <p class="card-text m-0 text-truncate" style="font-size: 0.5rem;">${data[i].content}</p>
                 </div>
-                <div class="modal-body">
-                  <div class="container-fluid">
-                    <div class="row">
-                      <div class="col-12">
-                          <img src="${data[i].img}" class=" card-img-top card-img-size" style="object-fit:cover">
-                      </div>
+              </div>
+            </div>
+
+            <div class="modal" id="e_c${+i}" tabindex="-1" style="background-color:rgb(255,255,255,0.5)">
+              <div class="modal-dialog modal-dialog-centered mx-auto modal-lg">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">${data[i].title}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     </div>
-                  </div>
-                  <div class="container">
-                    <div class="row h-100">
-                      <div class="col info fixed my-md-0 my-3">
-                        <div class="col h-75 overflow-auto bg-white border border-dark p-2 rounded mb-3">                          
-                          <h5>${data[i].title}</h5>
-                          <p>${data[i].content}</p>
-                        </div>
-                        <div class="col overflow-auto bg-white border border-dark p-2 rounded tags" style="height:10vh">
-                          123
+                    <div class="modal-body">
+                      <div class="container-fluid">
+                        <div class="row">
+                          <div class="col-12">
+                              <img src="${data[i].img}" class=" card-img-top card-img-size" style="object-fit:cover">
+                          </div>
                         </div>
                       </div>
-                      <div class="user fixed my-md-0 my-3 p-2">
-                        
-                        <div class="row justify-content-end align-items-center flex-column flex-xl-row flex-nowrap">
-                          <div class="col">
-                            <div class="h-100 d-flex flex-xl-column flex-row justify-content-center align-items-center">
-                              <div class="mr-3 mr-xl-0 userphoto"></div>
-                              <div class="userinfo bg-white p-2 mt-3 rounded border border-dark" style="width:150px">
-                                  <p>${userData}</p>
-                                  <p>${userData}</p>
-                                <hr>
-                                  <p>work:50</p>
-                                  <p>follower:20</p>
-                                  <p>following:100</p>
+                      <div class="container">
+                        <div class="row h-100">
+                          <div class="col info fixed my-md-0 my-3">
+                            <div class="col h-75 overflow-auto bg-white border border-dark p-2 rounded mb-3">                          
+                              <h5>${data[i].title}</h5>
+                              <p>${data[i].content}</p>
+                            </div>
+                            <div class="col overflow-auto bg-white border border-dark p-2 rounded tags d-flex align-items-center flex-wrap" style="max-height:10vh">
+                              123
+                            </div>
+                          </div>
+                          <div class="user fixed my-md-0 my-3 p-2">
+                            
+                            <div class="row justify-content-start align-items-center flex-column flex-xl-row flex-nowrap">
+                              <div class="col">
+                                <div class="h-100 d-flex flex-xl-column flex-row justify-content-center align-items-center">
+                                  <div class="mr-3 mr-xl-0 userphoto"></div>
+                                  <div class="userinfo bg-white p-2 mt-3 rounded border border-dark" style="width:150px">
+                                      <p>${userData[0].acc}</p>
+                                      <p>${userData[0].name}</p>
+                                    <hr>
+                                      <p>work:50</p>
+                                      <p>follower:20</p>
+                                      <p>following:100</p>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="col-auto d-flex align-items-center p-0 mr-xl-5">
+                                <div class="row flex-xl-column flex-row justify-content-center align-items-center">
+                                  <div class="col-auto m-3 bg-white border border-dark rounded-circle" style="width:50px;height:50px"></div>
+                                  <div class="col-auto m-3 bg-white border border-dark rounded-circle" style="width:50px;height:50px"></div>
+                                  <div class="col-auto m-3 bg-white border border-dark rounded-circle" style="width:50px;height:50px"></div>
+                                </div>
                               </div>
                             </div>
                           </div>
-                          <div class="col-auto d-flex align-items-center">
-                            <div class="row flex-xl-column flex-row justify-content-center align-items-center">
-                              <div class="col-auto m-3 bg-white border border-dark rounded-circle" style="width:50px;height:50px"></div>
-                              <div class="col-auto m-3 bg-white border border-dark rounded-circle" style="width:50px;height:50px"></div>
-                              <div class="col-auto m-3 bg-white border border-dark rounded-circle" style="width:50px;height:50px"></div>
-                            </div>
-                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
               </div>
-          </div>
-          </div>
-        `
-        $('#main').append(str) //插入卡片
+            </div>`
 
-        // 插入tags
-        let tagStr=''
-        let tagAry = data[i].tag.split(" ")
-        for (let j = 0; j < tagAry.length; j++) {
-          tagStr+= `<span class="badge badge-secondary mr-1">${tagAry[j]}</span>`
-        }
-        $('.modal').eq(i).find('.tags').html(tagStr)
+            $('#main').append(str) //插入卡片
 
-        let WoH='' //長寬比
-        let newimge = new Image();
-        newimge.src = data[i].img;
-        newimge.classList = "card-img-top shadow";
-        newimge.onload = function () {
-          let width = this.naturalWidth;
-          let height = this.naturalHeight;
-          WoH = width/height
-          console.log(WoH)
-          if (1<WoH) {
-            newimge.style = "border-radius: 5px;height: 200px;width:200px;object-fit: cover;";
-          }
-          if (0.75<WoH && WoH<=1) {
-            newimge.style = "border-radius: 5px;height: 200px;object-fit: cover;";
-          }
-          if (0.5<WoH && WoH<=0.75) {
-            $('.grid-item').eq(i).addClass('grid-item-h-3')
-            newimge.style = "border-radius: 5px;height: 300px;object-fit: cover;";
-          }
-          if (WoH<=0.5) {
-            $('.grid-item').eq(i).addClass('grid-item-h-4')
-            newimge.style = "border-radius: 5px;height: 400px;object-fit: cover;";
-          }
+            // 插入tags
+            let tagStr=''
+            let tagAry = data[i].tag.split(" ")
+            for (let j = 0; j < tagAry.length; j++) {
+              tagStr+= `<span class="badge badge-secondary mr-1 mb-1">${tagAry[j]}</span>`
+            }
+            $('.modal').eq(i).find('.tags').html(tagStr)
 
-          // masonry
-          $('.grid').masonry({
-            // options
-            itemSelector: '.grid-item',
-            columnWidth: 200,
-            gutter: 20,
-            fitWidth: true,
-          });
+            let WoH='' //長寬比
+            let newimge = new Image();
+            newimge.src = data[i].img;
+            newimge.classList = "card-img-top shadow";
+            newimge.onload = function () {
+              let width = this.naturalWidth;
+              let height = this.naturalHeight;
+              WoH = width/height
+              console.log(WoH)
+              if (1<WoH) {
+                newimge.style = "border-radius: 5px;height: 200px;width:200px;object-fit: cover;";
+              }
+              if (0.75<WoH && WoH<=1) {
+                newimge.style = "border-radius: 5px;height: 200px;object-fit: cover;";
+              }
+              if (0.5<WoH && WoH<=0.75) {
+                $('.grid-item').eq(i).addClass('grid-item-h-3')
+                newimge.style = "border-radius: 5px;height: 300px;object-fit: cover;";
+              }
+              if (WoH<=0.5) {
+                $('.grid-item').eq(i).addClass('grid-item-h-4')
+                newimge.style = "border-radius: 5px;height: 400px;object-fit: cover;";
+              }
 
-        };
+              // masonry
+              $('.grid').masonry({
+                // options
+                itemSelector: '.grid-item',
+                columnWidth: 200,
+                gutter: 20,
+                fitWidth: true,
+              });
 
-        $('.card a').eq(i).prepend(newimge)
+            };
+
+            $('.card a').eq(i).prepend(newimge)
+        })
       }
     
       $('.modal').on('shown.bs.modal',function () {
@@ -256,7 +254,6 @@ else {echo "$.getJSON('explore_api.php?do=init').done(showCard)";}
         if ($(this)[0].scrollHeight > t.height) {$(this).children().css('left',getScrollbarWidth())}
         else {$(this).children().css('left',getScrollbarWidth()/2)}
       })
-
 
     }
       
